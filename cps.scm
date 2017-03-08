93cc03801a3f6d3ff3cb6af11c0d2f0c13a21e70
刘宇辉 <emliunix@clojurians.org>
2017-03-08 22:24:14 +0800

    cps.scm使用counter手动生成名字不重复的参数名，然而还是没办法解决现有参数名可能造成的重复问题。

--- cps.scm
+++ cps.scm
@@ -1,13 +1,26 @@
 
+(define counter
+  ((lambda ()
+     (let [(i 0)]
+       (lambda ()
+	 (set! i (+ i 1))
+	 i)))))
 
-(define (mk-arg i)
-  (let [(p (open-output-string))]
+(define (mk-arg)
+  (let [(p (open-output-string))
+	(i (counter))]
     (display #\a p)
     (write i p)
     (string->symbol
      (get-output-string p))))
 
-(symbol->string (mk-arg 1))
+(define (mk-arg-fun)
+  (let [(p (open-output-string))
+	(i (counter))]
+    (display #\p p)
+    (write i p)
+    (string->symbol
+     (get-output-string p))))
 
 (define (convert exp cont)
   (cond [(pair? exp)
@@ -18,17 +31,17 @@
 
 (define (convert-call exp cont)
   (let [(p (car exp))
-	(args (cdr exp))]
+	(args (cdr exp))
+	(arg-p (mk-arg-fun))]
     (convert p
-	     `(lambda (p)
-	       ,(convert-args args 1 `(p ,cont))))))
+	     `(lambda (,arg-p)
+	       ,(convert-args args `(,arg-p ,cont))))))
 
-(define (convert-args args arg-index final)
+(define (convert-args args final)
   (if (null? args)
       final
-      (let [(a (mk-arg arg-index))]
+      (let [(a (mk-arg))]
 	(convert (car args)
 		 `(lambda (,a)
 		    ,(convert-args (cdr args)
-				  (+ arg-index 1)
-				  (append final `(,a))))))))
+				   (append final `(,a))))))))
